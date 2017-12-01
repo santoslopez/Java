0b67d1545534b49c8cea65363ddc4f6ec2b04ef1
Santos López <santos.lopez@galileo.edu>
2017-12-01 03:22:08 -0600

    Ejemplo básico de como usar Threads en java

--- Threads.java
+++ Threads.java
@@ -1,12 +1,14 @@
+//para usar hilos es importante poner extends Thread
 public class Threads extends Thread{
-
+  
+  //creando constructor para pasar un mensaje 
   public Threads (String mensaje){
     super(mensaje);
   }
 
-  //metodo que se ejecuta al momento de iniciar nuestro proceso
+  //metodo que se ejecuta al momento de iniciar nuestro proceso ---> el nombre del método es propio de los hilos
   public void run(){
-    //vamos a mostrar un mensaje mientras se cumpla el siguiente ciclo
+    //vamos a mostrar dos mensajes distintos mientras se cumpla el siguiente ciclo
     for (int i=0; i<100;i++ ) {
       System.out.println(this.getName());
     }
